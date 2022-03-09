261cd68e3a21889d67dae1364d55c7a1166b1dd1
Edward.Colon <edward.colon@noaa.gov>
2022-03-09 02:43:29 +0000

    Added updates to allow for the use of the bufr sounding utility in the HRRR-based 3D-RTMA.

--- build_sndp.sh
+++ build_sndp.sh
@@ -6,10 +6,12 @@ source ./machine-setup.sh > /dev/null 2>&1
 
 module purge >& /dev/null
 
-module use ~rtrr/RRFS/sorc/EMC_post/modulefiles
-module load post/v8.0.0-jet
-module use /lfs4/HFIP/hfv3gfs/gwv/l0530/lib/modulefiles
-module load bufr
+
+module use /gpfs/dell2/emc/modeling/noscrub/Edward.Colon/NOAA_3drtma_bec/sorc/rtma_post.fd/modulefiles
+module load post/v8.0.0-wcoss_dell_p3
+#module use /lfs4/HFIP/hfv3gfs/gwv/l0530/lib/modulefiles
+#module load bufr
+module load bufr/11.3.1
 
 module list
 
@@ -17,7 +19,7 @@ cd ./regional_sndp.fd
 
 
 make delete
-make FC=mpiifort
+make FC="mpif90 -f90=ifort"
 
 cd ../
 
